c73882807768e59cb442e8c9ae4d750e13d791ef
Larry <tradesouthwest@outlook.com>
2018-05-28 21:35:06 -0700

    js

---
+++ assets/jfunctions.js
@@ -0,0 +1,8 @@
+var currentdate = new Date(); 
+var datetime =  currentdate.getDate() + "/"
+                + (currentdate.getMonth()+1)  + "/" 
+                + currentdate.getFullYear() + " @ "  
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+    document.getElementById("Time").innerHTML = datetime;
