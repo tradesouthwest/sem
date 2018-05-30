8aa561acc210d25883cad1298b6911a8f76aad0b
Larry <tradesouthwest@outlook.com>
2018-05-29 21:18:32 -0700

    added copy link

--- assets/jfunctions.js
+++ assets/jfunctions.js
@@ -6,3 +6,17 @@ var datetime =  currentdate.getDate() + "/"
                 + currentdate.getMinutes() + ":" 
                 + currentdate.getSeconds();
     document.getElementById("Time").innerHTML = datetime;
+
+/**
+ * 1. var set
+ * 2. give the text a name
+ * 3. remove any formatting
+ * 4. copy to clipboard
+*/    
+function ClipBoard()
+{
+    holdtext.innerText = copytext.innerText;
+    Copied = holdtext.createTextRange();
+    Copied.execCommand("RemoveFormat");
+    Copied.execCommand("Copy");
+}
